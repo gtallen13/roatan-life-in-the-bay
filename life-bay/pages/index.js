7617be49b6aa240a7f94de2c4fbd20a8effb602b
gtallen13 <gtallenpadi13@gmail.com>
2021-10-25 16:44:00 -0600

    Carousel added with images and text

--- life-bay/pages/index.js
+++ life-bay/pages/index.js
@@ -1,11 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import stylesText from '../styles/Constants.module.css'
-
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import stylesText from '../styles/Constants.module.css';
+import Slider from '../components/Slider'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Home | Roatan Life in the Bay</title>
+        <meta name="keywords" content="home,roatan,tours,bay islands"></meta>
+      </Head>
+      {/* Banner Section */}
       <div className={styles.bannerContainer}>
         <Image className={styles.bannerImage} layout='fill' src="/banner3.jpg"/>
         <div className={styles.bannerText}>
@@ -13,7 +18,13 @@ export default function Home() {
                 <i className={stylesText.textSecondary}>Life in the Bay</i>
             </h1>
         </div>
-    </div>
+      </div>
+
+      {/* Home Section */}
+      <div className="home-section">
+        <h2 className={stylesText.textSecondary}> What our guests have to say about us...</h2>
+      </div>
+      <Slider/>
     </div>
   )
 }
