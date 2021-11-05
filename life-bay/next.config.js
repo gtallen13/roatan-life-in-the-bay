f95b44cc3ed943492e5c0ad0618444373775c2ed
gtallen13 <gtallenpadi13@gmail.com>
2021-11-04 18:14:19 -0600

    mongo uri added

--- life-bay/next.config.js
+++ life-bay/next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env:{
+    MONGO_URI: `mongodb+srv://gtallen13:0L81Y1Cm5oqL4wDg@cluster0.r1bha.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  }
 }
