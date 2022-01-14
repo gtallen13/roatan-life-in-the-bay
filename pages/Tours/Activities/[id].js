890b9433cc13697a044f6ef3ccd48886e39e5aed
gtallen13 <gtallenpadi13@gmail.com>
2022-01-14 10:13:09 -0600

    fixing parallax title position

--- pages/Tours/Activities/[id].js
+++ pages/Tours/Activities/[id].js
@@ -19,7 +19,7 @@ const Details = ({activity}) => {
                 <title>Tour the Island | Roatan Life in the Bay</title>
                 <meta name="keywords" content="tour,island,roatan,tours,bay islands"></meta>
             </Head> 
-            <div className="route-slider-conntainer">
+            <div>
                 <h1 className="title-parallax">{activity.name}</h1>
                 <Swiper 
                 className="banner-container"
